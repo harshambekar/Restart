b262a2ec5279f754e4f510962099c72d8f01aa6b
harshambekar <harshpendkar@gmail.com>
2024-07-06 17:47:40 +0530

    date  july

---
+++ 01/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+       <h1>Hello world</h1>
+       <p>lets have fun</p>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
